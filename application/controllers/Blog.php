1586862cb2e29e1dd2117c97f1e63d29530b29bd
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-09-10 01:41:35 +0700

    View

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -2,8 +2,10 @@
 
 class Blog extends CI_Controller{
     public function index($name, $blood_type, $address) {
-        echo "Nama : $name <br>";
-        echo "Golongan darah : $blood_type <br>";
-        echo "Alamat : $address <br>";
+        $data['name'] = $name;
+        $data['blood_type'] = $blood_type;
+        $data['address'] = $address;
+        
+        return $this->load->view('blog', $data);
     }
 }
---
+++ application/views/blog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+       <?php echo "Nama : $name <br>" ?>
+       <?php echo "Golongan darah : $blood_type <br>"?>
+       <?php echo "Alamat : $address <br>"?>
+</body>
+</html>
