708f82109bee4f2a08a1b8186c4b3ba86992a2d5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-09-10 13:18:51 +0700

    Akses Database

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,21 +1,12 @@
 <?php
 
 class Blog extends CI_Controller{
-    public function index() {
-        $data['blogs'] = [
-            [
-                'title' => "Pertama",
-                'description' => "Lorem ipsum dolor"
-            ],
-            [
-                'title' => "Kedua",
-                'description' => "Lorem ipsum dolor"
-            ],
-            [
-                'title' => "Tiga",
-                'description' => "Lorem ipsum dolor"
-            ]
-        ];
+    public function index() {    
+        $this->load->database();
+        
+        $query = $this->db->query("SELECT * FROM blogs");
+    
+        $data['blogs'] = $query->result_array();
         
         return $this->load->view('blog', $data);
     }
--- application/views/blog.php
+++ application/views/blog.php
@@ -8,7 +8,7 @@
 <body>
        <?php foreach ($blogs as $key => $blog): ?>
         <h1><?php echo $blog['title'] ?></h1>
-        <p><?php echo $blog['description'] ?></p>
-      <?php endforeach; ?>
+        <p><?php echo $blog['content'] ?></p>
+       <?php endforeach; ?>
 </body>
 </html>
